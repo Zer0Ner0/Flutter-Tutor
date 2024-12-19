63ca4442cdf512508bec20927f56ddb659311cac
Amir Mazlan <46234864+Zer0Ner0@users.noreply.github.com>
2024-12-19 23:21:03 +0800

    i am rich lesson 2 & 3

--- i_am_rich/android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
+++ i_am_rich/android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.i_am_rich
+package com.amirmz.i_am_rich
 
 import io.flutter.embedding.android.FlutterActivity
 
--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,9 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.red[50],
         appBar: AppBar(
-          title: Text(
-            'I Am Rich',
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.red[500],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
