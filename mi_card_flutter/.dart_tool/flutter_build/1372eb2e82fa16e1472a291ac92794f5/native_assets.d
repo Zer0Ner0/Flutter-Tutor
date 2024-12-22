685b75755e26751c7a0b3c138b6a10944ccd83ba
Amir Mazlan <46234864+Zer0Ner0@users.noreply.github.com>
2024-12-22 22:25:05 +0800

    Lesson 6 Container,column,row,fonts,icon,card,listTile

---
+++ mi_card_flutter/.dart_tool/flutter_build/1372eb2e82fa16e1472a291ac92794f5/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/zeronero/App\ Development/flutter-tutor/mi_card_flutter/.dart_tool/flutter_build/1372eb2e82fa16e1472a291ac92794f5/native_assets.yaml: 
---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/amir.jpg'),
+              ),
+              Text(
+                'Amir Mazlan',
+                style: TextStyle(
+                  fontFamily: 'Tangerine',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Source-San',
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+60 11 1628 3997',
+                    style: TextStyle(
+                      fontFamily: 'Fresca',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'amirmz48@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Fresca',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
